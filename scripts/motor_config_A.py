85f20ba6e9bcc9b55ff962d873c893550e2e1cf9
elXaptation <el@xaptation.com>
2020-05-22 23:57:52 +0000

    Updating default motor config.

--- scripts/motor_config_A.py
+++ scripts/motor_config_A.py
@@ -6,9 +6,9 @@ enableofflinePos = 33
 ## Motion Control
 servo_state = "disable"
 steps_per_revolution = 1600
-servo_angle = 15
-inspiratory_rate = .001
+servo_angle = 50
+inspiratory_rate = .002
 expiratory_rate = .005
-inspiratory_hold = .5
-expiratory_hold = .75
+inspiratory_hold = .25
+expiratory_hold = .5
 
