7345d020eececa19dc8a173ff0a49990a3765ce8
elXaptation <el@xaptation.com>
2020-06-09 14:09:46 -0700

    Initial RT changes for testing Arduino

---
+++ scripts/vent_rt_disable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "###### Modifying Arduino configuration"
+rosservice call /ventservo_rtsrv_config "{servo_state: false, steps_per_revolution: 1600, servo_angle: 60.0, inspiratory_rate: 2.0, expiratory_rate: 5.0, inspiratory_hold: 200.0, expiratory_hold: 300.0, publish_interval: 50}"
+
