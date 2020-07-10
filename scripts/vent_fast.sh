e74ab8802fdfc8acd59a02b78185b0cc793ab225
elXaptation <el@xaptation.com>
2020-07-10 09:34:34 -0700

    Fix init config logic and servo enable/disable logic

--- scripts/vent_fast.sh
+++ scripts/vent_fast.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "###### Modifying vent configuration"
-rosservice call /ventservo_srv_config "{type: 'configuration', steps_per_revolution: 1600, servo_angle: 55.0, inspiratory_rate: 1.0, expiratory_rate: 2.0, inspiratory_hold: 200.0, expiratory_hold: 300.0}"
+rosservice call /ventservo_srv_config "{type: 'configuration', steps_per_revolution: 1600, servo_angle: 55.0, inspiratory_rate: 2.0, expiratory_rate: 5.0, inspiratory_hold: 250.0, expiratory_hold: 350}"
 
--- scripts/vent_slow.sh
+++ scripts/vent_slow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "###### Modifying vent configuration"
-rosservice call /ventservo_srv_config "{type: 'configuration', steps_per_revolution: 1600, servo_angle: 55.0, inspiratory_rate: 2.0, expiratory_rate: 5.0, inspiratory_hold: 250.0, expiratory_hold: 500}"
+rosservice call /ventservo_srv_config "{type: 'configuration', steps_per_revolution: 1600, servo_angle: 55.0, inspiratory_rate: 3.0, expiratory_rate: 6.0, inspiratory_hold: 300.0, expiratory_hold: 500}"
 
